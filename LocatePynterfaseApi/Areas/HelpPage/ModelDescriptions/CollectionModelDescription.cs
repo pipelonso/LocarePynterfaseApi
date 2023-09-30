8076b760986eb0aacf88f81af1710c0c9adf8d79
pipelonso <andresfelipeibanezcuta2@gmail.com>
2023-09-29 23:27:42 -0500

    Agregar archivos de proyecto.

---
+++ LocatePynterfaseApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace LocatePynterfaseApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ LocatePynterfaseApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LocatePynterfaseApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
